b029abb1636c13d161b7256ac244983720961ea2
Erick Daniel Escoto Perez <erickdanielescoto@gmail.com>
2023-12-07 23:35:14 -0600

    archivoservice

--- drive/src/main/java/com/drive/drive/servicios/ArchivoService.java
+++ drive/src/main/java/com/drive/drive/servicios/ArchivoService.java
@@ -6,6 +6,23 @@ import com.drive.drive.modelos.Archivo;
 
 public interface ArchivoService {
     
-    public List<Archivo> obtenerTodosLosArchivos();
-    public Archivo guardarArchivo();
+
+    public List<Archivo> obtenerTodosLosArchivos(int idPropietario);
+
+    public List<Archivo> obtenerCarpetas (int idPropietario);
+
+    public List<Archivo> obtenerArchivosPorTipo (int idPropietario, int tipo);
+
+    public List<Archivo> obtenerArchivosPorPersonaPropietario (int idPropietario);
+
+    public List<Archivo> obtenerArchivosporFecha(int idPropietario, int idFecha);
+
+    public List<Archivo> obtenerArchivosporUbicacion(int idPropietario, int idUbicacion);
+
+    public List<Archivo> obtenerArchivosporEstado(int idPropietario, int idEstado);
+
+    public List<Archivo> obtenerArchivosporNombre(int idPropietario, String Nombre);
+
+    public List<Archivo> obtenerCarpetasporPersona(int idPropietario);
+    
 }
